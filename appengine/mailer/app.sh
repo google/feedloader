c0417480804d406a83d1aedcb7e7d719058fdbfd
Rihito Hashido <rihito@google.com>
2021-06-07 14:00:09 +0900

    Install pip package before running app.sh for mailer service.
    PiperOrigin-RevId: 377839603

--- appengine/mailer/app.sh
+++ appengine/mailer/app.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/bin/bash -u
+#!/bin/bash -eu
 CURRENT_DIRECTORY=$(dirname "$0")
 
 if [[ $# = 0 ]] || [[ $1 = 'dev' ]]; then
