5f369bf00103b3e63842ee135f713b4c9a062a25
Alex Kort <akort@google.com>
2021-06-16 16:13:30 +0900

    Internal change
    PiperOrigin-RevId: 379658491

--- cloud_functions/gcs-bq-feed-load/main.py
+++ cloud_functions/gcs-bq-feed-load/main.py
@@ -19,14 +19,16 @@ import logging
 import os
 from typing import Any, Dict
 
+from google.cloud import bigquery
 from google.cloud import exceptions
 from google.cloud import storage
 
 import iso8601
 import pytz
 
-# Set the default expiration of this Cloud Function to 9 minutes
+# Set the default expiration of this Cloud Function to 9 minutes.
 _EVENT_MAX_AGE_SECONDS = 540
+_ITEMS_TABLE_EXPIRATION_DURATION_MS = 43200000
 
 
 def import_storage_file_into_big_query(
@@ -37,38 +39,67 @@ def import_storage_file_into_big_query(
 
   Args:
       event:  The dictionary with data specific to this type of event. The
-        `data` field contains a description of the event in
-                     the Cloud Storage `object` format described here:
-                     https://cloud.google.com/storage/docs/json_api/v1/objects#resource
+        `data` field contains a description of the event in the Cloud Storage
+        `object` format described here:
+        https://cloud.google.com/storage/docs/json_api/v1/objects#resource
       context: Metadata of triggering event.
 
+  Raises:
+    RuntimeError: A dependency was not found, requiring this CF to exit.
+
   Returns:
-      None; the output is written to Stackdriver Logging.
+      None; the output is written to Cloud logging.
   """
 
   if _function_execution_exceeded_max_allowed_duration(context):
     return
 
   _log_uploaded_file_metadata(context, event)
-
-  storage_client = storage.Client()
-
   update_bucket_name = os.environ.get('UPDATE_BUCKET')
 
   if not update_bucket_name:
-    logging.error('Update Bucket Environment Variable was not found.')
+    logging.error(
+        exceptions.NotFound('Update Bucket Environment Variable not found.'))
     return
+
+  storage_client = storage.Client()
 
   try:
     eof_bucket = storage_client.get_bucket(update_bucket_name)
   except exceptions.NotFound:
-    logging.error('Bucket %s could not be found.', update_bucket_name)
+    logging.error(
+        exceptions.NotFound(f'Bucket {update_bucket_name} could not be found.'))
     return
 
   update_eof = eof_bucket.get_blob('EOF')
+
+  # The EOF file may be uploaded by the bq-stage-changes CF if processing is
+  # currently ongoing, so prevent this CF from continuing if it exists.
   if update_eof is not None:
-    logging.error('EOF file was found in bucket: %s', update_bucket_name)
+    logging.error(
+        RuntimeError((f'An EOF file was found in bucket: {update_bucket_name}, '
+                      'indicating Feedloader is currently processing '
+                      'a set of feeds into Content API. Please wait or '
+                      'force remove the EOF file from the bucket.')))
     return
+
+  # Cloud Functions executes before the file is visible in GCS, so check first.
+  if _file_to_import_exists(storage_client, event['bucket'], event['name']):
+    _perform_big_query_load(event['bucket'], event['name'])
+  else:
+    # Need to wait until the file is found, so raise to trigger an auto-retry.
+    raise RuntimeError((
+        f"GCS File {event['bucket']}/{event['name']} not detected in GCS yet. '"
+        f'Retrying...'))
+
+
+def _file_to_import_exists(storage_client: storage.client.Client,
+                           bucket_name: str, file_name: str) -> bool:
+  """Helper function that returns whether the given GCS file exists or not."""
+
+  storage_bucket = storage_client.bucket(bucket_name)
+  return storage.Blob(
+      bucket=storage_bucket, name=file_name).exists(storage_client)
 
 
 def _function_execution_exceeded_max_allowed_duration(
@@ -80,8 +111,10 @@ def _function_execution_exceeded_max_allowed_duration(
   event_age_seconds = (current_time - function_start_time).total_seconds()
 
   if event_age_seconds > _EVENT_MAX_AGE_SECONDS:
-    logging.error('Dropping event %s with age %d ms.', context.eventId,
-                  event_age_seconds)
+    logging.error(
+        RuntimeError(
+            f'Dropping event {context.event_id} with age {event_age_seconds} ms'
+        ))
     return True
   return False
 
@@ -90,16 +123,41 @@ def _log_uploaded_file_metadata(context: 'google.cloud.functions.Context',
                                 event: Dict[str, Any]) -> None:
   """Logs the Cloud Function event and file info to Stackdriver."""
 
-  logging.info('Event ID: %s', context.event_id)
-  logging.info('Event type: %s', context.event_type)
-  logging.info('Bucket: %s', event['bucket'])
-  logging.info('Uploaded Filename: %s', event['name'])
-  logging.info('Metageneration: %s', event['metageneration'])
-  logging.info('Created: %s', event['timeCreated'])
-  logging.info('Updated: %s', event['updated'])
+  print(f'Event ID: {context.event_id}')
+  print(f"Uploaded Filename: {event['bucket']}/{event['name']}")
 
 
 def _get_current_time_in_utc() -> datetime.datetime:
   """Helper function that wraps retrieving the current date and time in UTC."""
 
   return datetime.datetime.now(pytz.utc)
+
+
+def _perform_big_query_load(bucket_name: str, file_name: str) -> None:
+  """Helper function that handles the loading of the GCS file data into BQ."""
+
+  big_query_client = bigquery.Client()
+
+  big_query_job_config = bigquery.LoadJobConfig(
+      allow_jagged_rows=True,
+      autodetect=True,
+      encoding='UTF-8',
+      field_delimiter='\t',
+      quote_character='',
+      skip_leading_rows=1,
+      source_format=bigquery.SourceFormat.CSV,
+      time_partitioning=bigquery.table.TimePartitioning(
+          type_='DAY', expiration_ms=_ITEMS_TABLE_EXPIRATION_DURATION_MS),
+  )
+
+  gcs_uri = f'gs://{bucket_name}/{file_name}'
+  feed_table_path = f"{os.environ.get('BQ_DATASET')}.items"
+
+  big_query_load_job = big_query_client.load_table_from_uri(
+      gcs_uri, feed_table_path, job_config=big_query_job_config)
+
+  big_query_load_job.result()  # Waits for the job to complete.
+
+  destination_table = big_query_client.get_table(feed_table_path)
+  print('Loaded {} rows to table {}'.format(destination_table.num_rows,
+                                            feed_table_path))
--- cloud_functions/gcs-bq-feed-load/main_test.py
+++ cloud_functions/gcs-bq-feed-load/main_test.py
@@ -14,16 +14,25 @@
 # limitations under the License.
 
 """Unit tests for Feed File Import Function main.py."""
+import io
 import os
+
 import unittest
 import unittest.mock as mock
+
+from google.cloud import exceptions
+
 import iso8601
 import main
 
 _TEST_UPDATE_BUCKET = 'update-bucket'
+_TEST_BQ_DATASET = 'feed-dataset'
 
 
-@mock.patch.dict(os.environ, {'UPDATE_BUCKET': _TEST_UPDATE_BUCKET})
+@mock.patch.dict(os.environ, {
+    'UPDATE_BUCKET': _TEST_UPDATE_BUCKET,
+    'BQ_DATASET': _TEST_BQ_DATASET
+})
 @mock.patch('main._get_current_time_in_utc')
 class ImportStorageFileIntoBigQueryTest(unittest.TestCase):
 
@@ -37,22 +46,22 @@ class ImportStorageFileIntoBigQueryTest(unittest.TestCase):
         'timeCreated': '0',
         'updated': '0'
     }
-
     context = mock.MagicMock()
     context.event_id = '12345'
     context.event_type = 'gcs-event'
     context.timestamp = '2021-06-05T08:16:15.183Z'
-
     long_running_datetime = '2021-06-05T09:16:15.183Z'
+    mock_get_current_time_in_utc.return_value = iso8601.parse_date(
+        long_running_datetime)
 
     with mock.patch('main.storage.Client'), self.assertLogs(
-        level='ERROR') as mock_log:
-      mock_get_current_time_in_utc.return_value = iso8601.parse_date(
-          long_running_datetime)
+        level='ERROR') as mock_logging:
       main.import_storage_file_into_big_query(event, context)
-      self.assertIn('Dropping event', mock_log.output[0])
 
-  def test_import_storage_file_into_big_query_logs_file_import(
+      self.assertIn('Dropping event 12345', mock_logging.output[0])
+
+  @mock.patch.dict(os.environ, {'UPDATE_BUCKET': ''})
+  def test_import_storage_file_into_big_query_reports_error_on_missing_update_bucket_env_var(
       self, mock_get_current_time_in_utc):
     test_filename = 'feedfile'
     event = {
@@ -62,22 +71,21 @@ class ImportStorageFileIntoBigQueryTest(unittest.TestCase):
         'timeCreated': '0',
         'updated': '0'
     }
-
     context = mock.MagicMock()
     context.event_id = '12345'
     context.event_type = 'gcs-event'
     context.timestamp = '2021-06-05T08:16:15.183Z'
-
     mock_current_datetime = '2021-06-05T08:16:25.183Z'
+    mock_get_current_time_in_utc.return_value = iso8601.parse_date(
+        mock_current_datetime)
 
-    with mock.patch('main.storage.Client'), self.assertLogs(
-        level='INFO') as mock_log:
-      mock_get_current_time_in_utc.return_value = iso8601.parse_date(
-          mock_current_datetime)
+    with self.assertLogs(level='ERROR') as mock_logging:
       main.import_storage_file_into_big_query(event, context)
-      self.assertIn(f'Uploaded Filename: {test_filename}', mock_log.output[4])
 
-  def test_import_storage_file_into_big_query_calls_get_bucket(
+      self.assertIn('Update Bucket Environment Variable not found.',
+                    mock_logging.output[0])
+
+  def test_import_storage_file_into_big_query_reports_error_on_nonexistent_bucket(
       self, mock_get_current_time_in_utc):
     test_filename = 'feedfile'
     event = {
@@ -87,71 +95,163 @@ class ImportStorageFileIntoBigQueryTest(unittest.TestCase):
         'timeCreated': '0',
         'updated': '0'
     }
-
     context = mock.MagicMock()
     context.event_id = '12345'
     context.event_type = 'gcs-event'
     context.timestamp = '2021-06-05T08:16:15.183Z'
-
     mock_current_datetime = '2021-06-05T08:16:25.183Z'
-
-    with mock.patch('main.storage.Client') as mock_storage_client:
-      mock_get_current_time_in_utc.return_value = iso8601.parse_date(
-          mock_current_datetime)
-      main.import_storage_file_into_big_query(event, context)
-      mock_get_bucket = mock_storage_client.return_value.get_bucket
-      mock_get_bucket.assert_called_with(_TEST_UPDATE_BUCKET)
-
-  def test_import_storage_file_into_big_query_checks_for_eof(
-      self, mock_get_current_time_in_utc):
-    test_filename = 'feedfile'
-    event = {
-        'bucket': 'feed-bucket',
-        'name': test_filename,
-        'metageneration': 'test-metageneration',
-        'timeCreated': '0',
-        'updated': '0'
-    }
-
-    context = mock.MagicMock()
-    context.event_id = '12345'
-    context.event_type = 'gcs-event'
-    context.timestamp = '2021-06-05T08:16:15.183Z'
-
-    mock_current_datetime = '2021-06-05T08:16:25.183Z'
-
-    with mock.patch('main.storage.Client') as mock_storage_client:
-      mock_get_current_time_in_utc.return_value = iso8601.parse_date(
-          mock_current_datetime)
-      main.import_storage_file_into_big_query(event, context)
-      mock_get_bucket = mock_storage_client.return_value.get_bucket.return_value
-      mock_get_bucket.get_blob.assert_called_with('EOF')
-
-  def test_import_storage_file_into_big_query_logs_error_if_eof_exists(
-      self, mock_get_current_time_in_utc):
-    test_filename = 'feedfile'
-    event = {
-        'bucket': 'feed-bucket',
-        'name': test_filename,
-        'metageneration': 'test-metageneration',
-        'timeCreated': '0',
-        'updated': '0'
-    }
-
-    context = mock.MagicMock()
-    context.event_id = '12345'
-    context.event_type = 'gcs-event'
-    context.timestamp = '2021-06-05T08:16:15.183Z'
-
-    mock_current_datetime = '2021-06-05T08:16:25.183Z'
+    mock_get_current_time_in_utc.return_value = iso8601.parse_date(
+        mock_current_datetime)
 
     with mock.patch(
         'main.storage.Client') as mock_storage_client, self.assertLogs(
-            level='ERROR') as mock_log:
-      mock_get_current_time_in_utc.return_value = iso8601.parse_date(
-          mock_current_datetime)
+            level='ERROR') as mock_logging:
+      mock_storage_client.return_value.get_bucket.side_effect = exceptions.NotFound(
+          '404')
+
       main.import_storage_file_into_big_query(event, context)
+
+      self.assertIn('Bucket update-bucket could not be found.',
+                    mock_logging.output[0])
+
+  @mock.patch('main._file_to_import_exists')
+  @mock.patch('main._perform_big_query_load')
+  def test_import_storage_file_into_big_query_logs_file_import(
+      self, _, mock_file_to_import_exists, mock_get_current_time_in_utc):
+    test_filename = 'feedfile'
+    event = {
+        'bucket': 'feed-bucket',
+        'name': test_filename,
+        'metageneration': 'test-metageneration',
+        'timeCreated': '0',
+        'updated': '0'
+    }
+    context = mock.MagicMock()
+    context.event_id = '12345'
+    context.event_type = 'gcs-event'
+    context.timestamp = '2021-06-05T08:16:15.183Z'
+    mock_current_datetime = '2021-06-05T08:16:25.183Z'
+    mock_get_current_time_in_utc.return_value = iso8601.parse_date(
+        mock_current_datetime)
+
+    with mock.patch('main.storage.Client') as mock_storage_client, mock.patch(
+        'sys.stdout', new_callable=io.StringIO) as mock_stdout:
       mock_get_bucket = mock_storage_client.return_value.get_bucket.return_value
-      mock_get_bucket.get_blob.return_value = mock.MagicMock()
-      self.assertIn(f'EOF file was found in bucket: {_TEST_UPDATE_BUCKET}',
-                    mock_log.output[0])
+      mock_get_bucket.get_blob.return_value = None
+      mock_file_to_import_exists.return_value = True
+
+      main.import_storage_file_into_big_query(event, context)
+
+      self.assertIn(f'Uploaded Filename: feed-bucket/{test_filename}',
+                    mock_stdout.getvalue())
+
+  @mock.patch('main._file_to_import_exists')
+  @mock.patch('main._perform_big_query_load')
+  def test_import_storage_file_into_big_query_calls_get_bucket(
+      self, _, mock_file_to_import_exists, mock_get_current_time_in_utc):
+    test_filename = 'feedfile'
+    event = {
+        'bucket': 'feed-bucket',
+        'name': test_filename,
+        'metageneration': 'test-metageneration',
+        'timeCreated': '0',
+        'updated': '0'
+    }
+    context = mock.MagicMock()
+    context.event_id = '12345'
+    context.event_type = 'gcs-event'
+    context.timestamp = '2021-06-05T08:16:15.183Z'
+    mock_current_datetime = '2021-06-05T08:16:25.183Z'
+    mock_get_current_time_in_utc.return_value = iso8601.parse_date(
+        mock_current_datetime)
+
+    with mock.patch('main.storage.Client') as mock_storage_client:
+      mock_get_bucket = mock_storage_client.return_value.get_bucket
+      mock_get_bucket.return_value.get_blob.return_value = None
+      mock_file_to_import_exists.return_value = True
+
+      main.import_storage_file_into_big_query(event, context)
+
+      mock_get_bucket.assert_called_with(_TEST_UPDATE_BUCKET)
+
+  @mock.patch('main._file_to_import_exists')
+  @mock.patch('main._perform_big_query_load')
+  def test_import_storage_file_into_big_query_checks_for_eof(
+      self, _, mock_file_to_import_exists, mock_get_current_time_in_utc):
+    test_filename = 'feedfile'
+    event = {
+        'bucket': 'feed-bucket',
+        'name': test_filename,
+        'metageneration': 'test-metageneration',
+        'timeCreated': '0',
+        'updated': '0'
+    }
+    context = mock.MagicMock()
+    context.event_id = '12345'
+    context.event_type = 'gcs-event'
+    context.timestamp = '2021-06-05T08:16:15.183Z'
+    mock_current_datetime = '2021-06-05T08:16:25.183Z'
+    mock_get_current_time_in_utc.return_value = iso8601.parse_date(
+        mock_current_datetime)
+
+    with mock.patch('main.storage.Client') as mock_storage_client:
+      mock_get_bucket = mock_storage_client.return_value.get_bucket.return_value
+      mock_get_bucket.get_blob.return_value = None
+      mock_file_to_import_exists.return_value = True
+
+      main.import_storage_file_into_big_query(event, context)
+
+      mock_get_bucket.get_blob.assert_called_with('EOF')
+
+  def test_import_storage_file_into_big_query_throws_error_if_eof_exists(
+      self, mock_get_current_time_in_utc):
+    test_filename = 'feedfile'
+    event = {
+        'bucket': 'feed-bucket',
+        'name': test_filename,
+        'metageneration': 'test-metageneration',
+        'timeCreated': '0',
+        'updated': '0'
+    }
+    context = mock.MagicMock()
+    context.event_id = '12345'
+    context.event_type = 'gcs-event'
+    context.timestamp = '2021-06-05T08:16:15.183Z'
+    mock_current_datetime = '2021-06-05T08:16:25.183Z'
+    mock_get_current_time_in_utc.return_value = iso8601.parse_date(
+        mock_current_datetime)
+
+    with mock.patch('main.storage.Client'), self.assertLogs(
+        level='ERROR') as mock_logging:
+      main.import_storage_file_into_big_query(event, context)
+
+      self.assertIn('An EOF file was found in bucket', mock_logging.output[0])
+
+  def test_import_storage_file_into_big_query_loads_data_to_bq_successfully(
+      self, mock_get_current_time_in_utc):
+    test_filename = 'feedfile'
+    event = {
+        'bucket': 'feed-bucket',
+        'name': test_filename,
+        'metageneration': 'test-metageneration',
+        'timeCreated': '0',
+        'updated': '0'
+    }
+    context = mock.MagicMock()
+    context.event_id = '12345'
+    context.event_type = 'gcs-event'
+    context.timestamp = '2021-06-05T08:16:15.183Z'
+    mock_current_datetime = '2021-06-05T08:16:25.183Z'
+    mock_get_current_time_in_utc.return_value = iso8601.parse_date(
+        mock_current_datetime)
+
+    with mock.patch('main.storage.Client') as mock_storage_client, mock.patch(
+        'main.bigquery.Client') as mock_bigquery_client:
+
+      mock_get_bucket = mock_storage_client.return_value.get_bucket.return_value
+      mock_get_bucket.get_blob.return_value = None
+      main.import_storage_file_into_big_query(event, context)
+      mock_bigquery_client.return_value.load_table_from_uri.assert_called_with(
+          'gs://feed-bucket/feedfile',
+          f'{_TEST_BQ_DATASET}.items',
+          job_config=mock.ANY)
